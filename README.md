5ab38b7d372a327b5bf37f5fe5d2279c434e0dc0
chrisdwells <c.d.wells@leeds.ac.uk>
2025-03-12 20:08:38 +0000

    1st 3 impacts

---
+++ README.md
@@ -0,0 +1,11 @@
+This repository is to contain any processing needed related to multiple climate impacts in FRIDA.
+
+Currently for FRIDAv2.1 it just collates the uncertainty parameter sets for the climate impacts which are
+literature-derived with uncertainty.
+
+The output pickle files need to be brought in from the repos which produce them for each impact separately:
+https://github.com/chrisdwells/climate-energy-supply
+https://github.com/chrisdwells/temperature-mortality
+https://github.com/chrisdwells/extremes-exposure
+
+The code here assumes these repos are kept in the same place as this one.
---
+++ scripts/00_collate_parameter_sets.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+percentiles = np.linspace(0.025, 0.975, 11)
+
+
+pkl_files = [
+    '../../climate-energy-supply/data/outputs/energy_suppy_output_dict.pickle',
+    '../../temperature-mortality/data/outputs/mortality_output_dict.pickle',
+    '../../extremes-exposure/data/outputs/exposure_output_dict.pickle'
+    ]
+
+columns = list(percentiles)
+columns.insert(0, 'Variable')
+
+df = pd.DataFrame(columns = columns)
+
+for f in pkl_files:
+    with open(f, 'rb') as handle:
+        pkl_in = pickle.load(handle)
+      
+    assert pkl_in['Percentiles'].all() == percentiles.all()
+    
+    for var in pkl_in.keys():
+        if var != 'Percentiles':
+            
+            row_in = list(pkl_in[var])
+            row_in.insert(0, var)
+            
+            df.loc[len(df)] = row_in
+
+
+#df = df.T.reset_index(drop=True).T
+
+df.to_csv('../outputs/climate_impacts_parameter_sets.csv', index=False)
+
+
+
